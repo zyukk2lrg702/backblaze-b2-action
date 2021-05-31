030e005c9fd9116794a068a547645e33a0078d79
zyukk2lrg702 <85069529+zyukk2lrg702@users.noreply.github.com>
2021-05-31 11:37:12 +0300

    shellcheck entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,9 +22,9 @@ if [ -z "$SOURCE_DIR" ]; then
   exit 1
 fi
 
-b2 authorize-account ${B2_APPKEY_ID} ${B2_APPKEY}
+b2 authorize-account "${B2_APPKEY_ID}" "${B2_APPKEY}"
 
-b2 sync --delete --replaceNewer ${SOURCE_DIR} ${B2_BUCKET}
+b2 sync --delete --replaceNewer "${SOURCE_DIR}" "${B2_BUCKET}"
 
 # TO-DO: Delete old versions of updated files because that's what
 # we're already using GitHub for... but B2 doesn't make this easy:
